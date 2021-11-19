e741f456065c8a8ff1691d160d2a1d85478c1619
Southier <patrick.southier@hotmail.com>
2021-11-18 23:18:35 -0300

    feat: add a validation to not allow empty entries at email or password requests

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -1,5 +1,12 @@
 const { StatusCodes } = require('http-status-codes');
 
+const EMPTY_ENTRY = 0;
+
+const isEmailValid = (email) => {
+  const emailPattern = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+  return emailPattern.test(email);
+};
+
 const validateName = (req, res, next) => {
   const { displayName } = req.body;
   const MIN_NAME_LENGTH = 8;
@@ -15,8 +22,12 @@ const validateName = (req, res, next) => {
 
 const validateEmail = (req, res, next) => {
   const { email } = req.body;
-  const emailPattern = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-  const isEmailValid = emailPattern.test(email);
+  
+  if (email.length === EMPTY_ENTRY) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: '"email" is not allowed to be empty',
+    });
+  }
 
   if (!email) {
     return res.status(StatusCodes.BAD_REQUEST).json({
@@ -24,7 +35,7 @@ const validateEmail = (req, res, next) => {
     });
   }
 
-  if (!isEmailValid) {
+  if (!isEmailValid(email)) {
     return res.status(StatusCodes.BAD_REQUEST).json({
       message: '"email" must be a valid email',
     });
@@ -36,6 +47,12 @@ const validateEmail = (req, res, next) => {
 const validatePassword = (req, res, next) => {
   const { password } = req.body;
   const PASSWORD_LENGTH = 6;
+
+  if (password.length === EMPTY_ENTRY) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: '"password" is not allowed to be empty',
+    });
+  }
 
   if (!password) {
     return res.status(StatusCodes.BAD_REQUEST).json({
